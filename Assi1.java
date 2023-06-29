c845fdab9ebd742b92355b713ceff4da7ec3ad7f
paresh_chandel <125260576+PareshsinhChandel@users.noreply.github.com>
2023-06-29 15:37:19 +0530

    Add files via upload

---
+++ Assi1.java
@@ -0,0 +1,16 @@
+package Strpkg;
+
+public class Assi1 {
+
+	public static void main(String[] args) {
+	
+	    String str="Paresh";
+		
+	    for(int i=0;i<str.length();i++)
+	    {
+	    	System.out.println(str.charAt(i));
+	    }
+
+	}
+
+}
